48c955e7bfb13d7baaf39c4273d4c48c9f59ecbd
Irvan Fauziansyah <ervhan@gmail.com>
2021-04-04 21:14:50 +0700

    Add FizzBuzz implementation

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,5 +2,12 @@
 
 class FizzBuzz
   def self.divisible_by(input)
+    if input % 3 == 0 && input % 5 == 0
+      return 'FizzBuzz'
+    elsif input % 3 == 0
+      return 'Fizz'
+    elsif input % 5 == 0
+      return 'Buzz'
+    end
   end
 end
