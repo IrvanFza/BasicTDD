fd91b0cde3038dee6913aa6a7c65e09a60425a1c
Irvan Fauziansyah <ervhan@gmail.com>
2021-04-04 21:37:27 +0700

    Refactor FizzBuzz test case and implementation

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class FizzBuzz
+  FIZZ = 'Fizz'
+  BUZZ = 'Buzz'
+
   def self.divisible_by(input)
-    if input % 3 == 0 && input % 5 == 0
-      return 'FizzBuzz'
-    elsif input % 3 == 0
-      return 'Fizz'
-    elsif input % 5 == 0
-      return 'Buzz'
-    else
-      return input
-    end
+    fizz = (input % 3 == 0)
+    buzz = (input % 5 == 0)
+    return case
+             when fizz && buzz then FIZZ + BUZZ
+             when fizz then FIZZ
+             when buzz then BUZZ
+             else input
+           end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,29 +4,38 @@ require './lib/fizzbuzz'
 
 describe FizzBuzz do
   context 'when given an input' do
-    it 'returns Fizz if divisible by 3' do
-      result = FizzBuzz.divisible_by(3)
-      expect(result).to eq('Fizz')
+    let(:fizz_buzz) { FizzBuzz }
+    fizz = 'Fizz'
+    buzz = 'Buzz'
+
+    context 'returns word' do
+      it "#{fizz} if divisible by 3" do
+        result = fizz_buzz.divisible_by(3)
+        expect(result).to eq(fizz)
+      end
+      it "#{buzz} if divisible by 5" do
+        result = fizz_buzz.divisible_by(5)
+        expect(result).to eq(buzz)
+      end
+      it "#{fizz+buzz} if divisible by 3 and 5" do
+        result = fizz_buzz.divisible_by(15)
+        expect(result).to eq(fizz+buzz)
+      end
     end
-    it 'returns Buzz if divisible by 5' do
-      result = FizzBuzz.divisible_by(5)
-      expect(result).to eq('Buzz')
-    end
-    it 'returns FizzBuzz if divisible by 3 and 5' do
-      result = FizzBuzz.divisible_by(15)
-      expect(result).to eq('FizzBuzz')
-    end
-    it 'returns the input if not divisible by 3' do
-      result = FizzBuzz.divisible_by(1)
-      expect(result).to eq(1)
-    end
-    it 'returns the input if not divisible by 5' do
-      result = FizzBuzz.divisible_by(1)
-      expect(result).to eq(1)
-    end
-    it 'returns the input if not divisible by 3 and 5' do
-      result = FizzBuzz.divisible_by(1)
-      expect(result).to eq(1)
+
+    context 'returns the input' do
+      it 'if not divisible by 3' do
+        result = fizz_buzz.divisible_by(1)
+        expect(result).to eq(1)
+      end
+      it 'if not divisible by 5' do
+        result = fizz_buzz.divisible_by(1)
+        expect(result).to eq(1)
+      end
+      it 'if not divisible by 3 and 5' do
+        result = fizz_buzz.divisible_by(1)
+        expect(result).to eq(1)
+      end
     end
   end
 end
